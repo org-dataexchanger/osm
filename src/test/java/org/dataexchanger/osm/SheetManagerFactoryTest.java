f25a8263cae1b75116143dcfb82c382ffeee1939
mainul35-bs23 <mainul.hasan@bs-23.net>
2020-06-28 23:09:37 +0600

    SheetManagerFactoryTest added

---
+++ src/test/java/org/dataexchanger/osm/SheetManagerFactoryTest.java
@@ -0,0 +1,39 @@
+package org.dataexchanger.osm;
+
+import org.dataexchanger.osm.example.Address;
+import org.dataexchanger.osm.example.Employee;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SheetManagerFactoryTest {
+
+
+    private SheetManagerFactory sheetManagerFactory;
+    @Before
+    public void setup() throws IOException, ClassNotFoundException {
+        SheetManager sheetManagerBean = new SheetManagerBean();
+        sheetManagerBean.scanMappedPackages("org.dataexchanger.osm.example");
+        sheetManagerFactory = new SheetManagerFactory(sheetManagerBean);
+    }
+
+    @Test
+    public void test_export() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, NoSuchFieldException {
+        List<Employee> employees = new ArrayList<Employee>();
+        Employee e = new Employee();
+        e.setName("Mainul");
+        e.setAge("25");
+        Address address = new Address();
+        address.setId(3);
+        address.setHouse("Forest Lodge");
+        address.setStreet("S.S. Academy Road");
+        e.setAddress(address);
+        employees.add(e);
+
+        sheetManagerFactory.export(employees);
+    }
+}
